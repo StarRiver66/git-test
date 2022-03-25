0860c437341d1b76480dcfd6ca13ffd0ec5902f7
zzy <Xiao.ye_Zhang@qq.com>
2022-03-25 08:44:33 +0800

    first commit

---
+++ src/main/java/com/zzy/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.zzy.git;
+
+/**
+ * @author 星河、
+ * @version 1.0
+ * @introduction 努力总不会错的对吧，宝~
+ */
+@SuppressWarnings({"all"})
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello.git");
+    }
+}
